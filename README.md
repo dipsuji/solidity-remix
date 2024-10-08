94fbee42dd35dae40111891e6af6a278cc4a69c3
Sujata <mauryasujata69@gmail.com>
2024-10-08 12:49:20 +0900

    setup, start, stop with smart contract

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # solidity-remix
 Solidity Basic for Learning Smart Contract with Remix IDE
+
+### online editior - 
+https://remix.ethereum.org/
+
+### Chapter
+1. **MyContract.sol**
+        -  Starting, Stopping and Interacting with Smart Contract.
---
+++ contracts/MyContract.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.14;
+
+contract MyContract {
+
+    string public ourString = "Hello World";
+
+}
+
+//  Starting, Stopping and Interacting with Smart Contracts¶
+//https://ethereum-blockchain-developer.com/2022-01-remix-introduction/02-starting-stopping-interacting/?coupon=udemy_77288

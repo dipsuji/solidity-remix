4468ed50741b7a99a499ab0dbc483bb8996188bc
Sujata <mauryasujata69@gmail.com>
2024-10-09 11:55:13 +0900

    boolean,integer, Integer Rollover¶

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -6,7 +6,14 @@ contract MyContract {
 
     string public ourString = "Hello World";
 
+    function updateOurString(string memory _updateString) public {
+        ourString = _updateString;
+    }
+
 }
 
-//  Starting, Stopping and Interacting with Smart Contracts¶
+//  1- Starting, Stopping and Interacting with Smart Contracts¶
 //https://ethereum-blockchain-developer.com/2022-01-remix-introduction/02-starting-stopping-interacting/?coupon=udemy_77288
+
+// 2- Read and Write to Smart Contract¶
+// https://ethereum-blockchain-developer.com/2022-01-remix-introduction/04-read-write-to-smart-contract/?coupon=udemy_77288
